8deb33550a334be634d40f483787776252bf2782
Camilla-Fartun Jamal <100544668+Camjamal@users.noreply.github.com>
2022-10-12 23:11:13 -0400

    MultiplicationApp
    A simple Calculator for my kids

---
+++ index.js
@@ -0,0 +1,43 @@
+const num1 = Math.ceil (Math.random() * 11)
+
+const num2 = Math.ceil (Math.random() * 11)
+
+const questionEl = document.getElementById("question");
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}? `
+
+
+const correctAns = num1 * num2;
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if(!score){
+  score = 0;
+}
+
+const scoreEl = document.getElementById("score")
+
+scoreEl. innerText = `score: ${score}`
+
+const formEl = document.getElementById("form")
+const inputEl = document.getElementById("input")
+
+formEl.addEventListener("submit", ()=>{
+  const userAns = +inputEl.value
+  if(userAns === correctAns){
+    score++
+    updateLocalStorage()
+
+  }
+  else{
+    score--
+    updateLocalStorage()
+  }
+
+});
+
+function updateLocalStorage(){
+  localStorage.setItem("score", JSON.stringify(score))
+}
+
+
